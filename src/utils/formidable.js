7d674a4545531b68c2037be6f448aac950d9b818
Dinh Canh <dinhcanhh2004@gmail.com>
2025-11-08 23:16:03 +0700

    feat: add formidable utils

---
+++ src/utils/formidable.js
@@ -0,0 +1,12 @@
+import formidable from "formidable";
+
+export const parseForm = (req) => {
+  const form = formidable();
+
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      else resolve({ fields, files });
+    });
+  });
+};
